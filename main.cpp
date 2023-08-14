2b3bbe1047f83b3a63cde487713eb8741c5f8043
itay.guter <ithayg@gmail.com>
2023-08-14 14:28:54 +0300

    setup instance

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 #include <iostream> // for errors
 #include <stdexcept> // std::exception
 #include <cstdlib> // EXIT_FAILURE, EXIT_SUCCESS
+#include <vector> // std::vector
+#include <algorithm> // std::find
+#include <cstring> // strcmp
 
 class TriangleApp
 {
@@ -14,12 +17,17 @@ public:
     void Run();
 
 private:
-    GLFWwindow *m_window;
+    GLFWwindow *m_window{nullptr};
+    VkInstance m_instance;
 
     void InitWindow();
     void InitVulkan();
+    void CreateInstance();
     void MainLoop();
     void Cleanup();
+
+    static bool CheckExstensions(const char **glfwExtensions, 
+                                    uint32_t glfwExtensionCount);
 
     static constexpr uint32_t WIDTH = 800;
     static constexpr uint32_t HEIGHT = 600;
@@ -62,7 +70,76 @@ void TriangleApp::InitWindow()
 
 void TriangleApp::InitVulkan()
 {
+    CreateInstance();   
 
+}
+
+void TriangleApp::CreateInstance()
+{
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pNext = nullptr;
+    appInfo.pApplicationName = "Vulkan Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+    
+    uint32_t glfwExtensionCount = 0;
+    const char **glfwExtensions = 
+    glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+    createInfo.enabledLayerCount = 0;
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    if (!CheckExstensions(glfwExtensions, glfwExtensionCount))
+    {
+        throw std::runtime_error("not all extensions are supported");
+    }
+
+    if (VK_SUCCESS != vkCreateInstance(&createInfo, nullptr, &m_instance))
+    {
+        throw std::runtime_error("failed to create instance");
+    }
+
+}
+
+bool TriangleApp::CheckExstensions(const char **glfwExtensions, 
+                                    uint32_t glfwExtensionCount)
+{
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    std::vector<VkExtensionProperties> supportedExtensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, 
+                                            supportedExtensions.data());
+    std::cout << "Available extensions " << extensionCount << ":" << std::endl;
+
+    for (const auto& extension : supportedExtensions)
+    {
+        std::cout << '\t' << extension.extensionName << '\n';
+    }
+
+    for (uint32_t i = 0; i < glfwExtensionCount; ++i)
+    {
+        if (std::find_if(supportedExtensions.begin(), supportedExtensions.end(),
+            [glfwExtensions, i](const auto& extension)
+            {
+                return (0 == std::strcmp(extension.extensionName, 
+                                        glfwExtensions[i]));
+            })  == supportedExtensions.end())
+        {
+            return false;
+        }
+    }
+
+    std::cout << "All " << glfwExtensionCount << 
+    " GLFW extensions are supported" << std::endl;
+
+    return true;
 }
 
 void TriangleApp::MainLoop()
@@ -75,6 +152,8 @@ void TriangleApp::MainLoop()
 
 void TriangleApp::Cleanup()
 {
+    vkDestroyInstance(m_instance, nullptr);
+    
     glfwDestroyWindow(m_window);
 
     glfwTerminate();
