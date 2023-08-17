8a1f9478e25f49a73439cd98ebbffdeeb45623c0
itay.guter <ithayg@gmail.com>
2023-08-17 11:34:22 +0300

    image views

--- main.cpp
+++ main.cpp
@@ -34,6 +34,7 @@ private:
     std::vector<VkImage> m_swapChainImages;
     VkFormat m_swapChainImageFormat;
     VkExtent2D m_swapChainExtent;
+    std::vector<VkImageView> m_swapChainImageViews;
 
     void InitWindow();
     void InitVulkan();
@@ -43,6 +44,7 @@ private:
     void PickPhysicalDevice();
     void CreateLogicalDevice();
     void CreateSwapChain();
+    void CreateImageViews();
     void MainLoop();
     void Cleanup();
 
@@ -162,6 +164,7 @@ void TriangleApp::InitVulkan()
     PickPhysicalDevice();
     CreateLogicalDevice();
     CreateSwapChain();
+    CreateImageViews();
 
 }
 
@@ -424,6 +427,35 @@ void TriangleApp::CreateSwapChain()
     m_swapChainExtent = extent;
 }
 
+void TriangleApp::CreateImageViews()
+{
+    m_swapChainImageViews.resize(m_swapChainImages.size());
+
+    for (std::size_t i = 0; i < m_swapChainImages.size(); ++i)
+    {
+        VkImageViewCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+        createInfo.image = m_swapChainImages[i];
+        createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        createInfo.format = m_swapChainImageFormat;
+        createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        createInfo.subresourceRange.baseMipLevel = 0;
+        createInfo.subresourceRange.levelCount = 1;
+        createInfo.subresourceRange.baseArrayLayer = 0;
+        createInfo.subresourceRange.layerCount = 1;
+
+        if (VK_SUCCESS != vkCreateImageView(m_device, &createInfo, 
+                                nullptr, &m_swapChainImageViews[i]))
+        {
+            throw std::runtime_error("failed to create image views");
+        }
+    }
+}
+
 void TriangleApp::MainLoop()
 {
     while (!glfwWindowShouldClose(m_window))
@@ -434,6 +466,11 @@ void TriangleApp::MainLoop()
 
 void TriangleApp::Cleanup()
 {
+    for (VkImageView imageView : m_swapChainImageViews)
+    {
+        vkDestroyImageView(m_device, imageView, nullptr);
+    }
+    
     vkDestroySwapchainKHR(m_device, m_swapChain, nullptr);
     vkDestroyDevice(m_device, nullptr);
     
